95b69f1891167c668037a605c791ceb00ba2cbc3
ykb <jakubhlavacka123@gmail.com>
2023-08-06 19:08:15 +0200

    remove searched entity from orgs and names in article

--- blueprints/adverse_entity.py
+++ blueprints/adverse_entity.py
@@ -10,6 +10,34 @@ adverse_entity = Blueprint('adverse_entity', __name__, url_prefix='/adverse-enti
 graph = Graph(f'bolt://{settings.NEO4J_BOLT_URL}:{settings.NEO4J_BOLT_PORT}',
             auth=(settings.NEO4J_USERNAME, settings.NEO4J_PASSWORD))
 
+
+def remove_lower_name_from_lists(lower_name, article_data, ascii_column, non_ascii_column):
+    index = article_data[ascii_column].index(lower_name)
+    article_data[ascii_column].pop(index)
+    article_data[non_ascii_column].pop(index)
+
+    return article_data
+
+
+def remove_search_ent_from_orgs_and_names(lower_name, cursor_data):
+    articles = []
+
+    for record in cursor_data:
+        article_data = record['article']
+
+        if lower_name in article_data['gpt3_names_ascii']:
+            article_data = remove_lower_name_from_lists(lower_name, article_data,
+                                                        'gpt3_names_ascii', 'gpt3_names')
+
+        if lower_name in article_data['gpt3_organizations_ascii']:
+            article_data = remove_lower_name_from_lists(lower_name, article_data, 
+                                                        'gpt3_organizations_ascii', 'gpt3_organizations')
+
+        articles.append(article_data)
+
+    return articles
+
+
 @adverse_entity.route('/search', methods=['GET'])
 def search():
     name = request.args.get('name', type=str)
@@ -33,7 +61,8 @@ def detail():
             'message': 'Provide "name" parameter'
         }), 400
 
-    cursor = graph.run(adv_ent_articles_query, lower_name=unidecode(name.lower()))
-    query_data = [dict(record['article']) for record in cursor.data()]
+    lower_name = unidecode(name.lower())
+    cursor = graph.run(adv_ent_articles_query, lower_name=lower_name)
+    query_data = remove_search_ent_from_orgs_and_names(lower_name, cursor.data())
 
     return jsonify(query_data), 200
